e493ed0a2c13acfcc1bea8ce1e39e89f1081614b
BildPeter <BildPeter@web.de>
2013-06-24 15:29:25 +0200

    GNU License included

--- Adaptor.cpp
+++ Adaptor.cpp
@@ -3,7 +3,21 @@
 //  MinCostFlow_Milk
 //
 //  Created by Peter A. Kolski (BildPeter Visuals) A. Kolski on 21.10.11.
-//  Copyright 2011 BildPeter Visuals. All rights reserved.
+//  Copyright (c) 2011 Peter A. Kolski.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  For the GNU General Public License see <http://www.gnu.org/licenses/>.
+//
+//
 //
 
 #include <iostream>
--- src/Template.cpp
+++ src/Template.cpp
@@ -3,7 +3,21 @@
 //  LEMON_GAME
 //
 //  Created by Peter A. Kolski (BildPeter Visuals) on 16.11.12.
-//  Copyright (c) 2012 Peter A. Kolski. All rights reserved.
+//  Copyright (c) 2012 Peter A. Kolski.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  For the GNU General Public License see <http://www.gnu.org/licenses/>.
+//
+//
 //
 
 
